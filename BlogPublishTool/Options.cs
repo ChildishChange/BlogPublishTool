4979aa7cc5fa95e7caf9403d4fd4183028bfeca8
Yanji Chen <v-yanch@microsoft.com>
2018-07-19 18:26:54 +0800

    add reference to nuget packages: commandline parser , metaweblogclient.core

---
+++ BlogPublishTool/Options.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace BlogPublishTool
+{
+    class Options
+    {
+        [Option('i', "input",  HelpText = "Input file to read.")]
+        public string InputFile { get; set; }
+
+        [Option('l',"length", HelpText = "The maximum number of bytes to process.")]
+        public int MaximumLength { get; set; }
+
+        [Option('v',  HelpText = "Print details during execution.")]
+        public bool Verbose { get; set; }
+        
+    }
+}
--- BlogPublishTool/Program.cs
+++ BlogPublishTool/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MetaWeblogClient;
+using CommandLine;
 
 //init
 namespace BlogPublishTool
@@ -12,6 +13,17 @@ namespace BlogPublishTool
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var result = Parser.Default.ParseArguments<Options>(args);
+                Console.WriteLine(result.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadKey();
         }
     }
 }
