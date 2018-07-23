9ed2947fdaf8a66361a918b7e31dd817d8136e87
ChildishChange <457694145@qq.com>
2018-07-23 17:01:10 +0800

    prepare for new command : app.exe replace --input xxx --output yyy --config zzz app.exe uploadpic --input xxx --test(optional)

--- BlogPublishTool/Options.cs
+++ BlogPublishTool/Options.cs
@@ -19,16 +19,24 @@ namespace BlogPublishTool
     [Verb("replace", HelpText = "Replace link of picture in the blog")]
     public class ReplaceOptions
     {
+        //deal with single file
         [Option("picture", Required = false, Default = false, HelpText = "Upload and replace picture with URL.")]
         public bool PictureFlag { get; set; }
-        
-        [Option("link", Required = false, HelpText = "Replace link with URL.")]
-        public string LinkJsonPath { get; set; }
         
         [Option("cnblogs", Required = false, HelpText = "Replace the link with blog on cnblogs.")]
         public string CnblogsFilePath { get; set; }
 
         [Option("csdn", Required = false, HelpText = "Replace the link with the blog on csdn.")]
         public string CsdnFilePath { get; set; }
+
+        [Option("config", Required = false, HelpText = "Config json file path.")]
+        public string LinkJsonPath { get; set; }
+
+        //deal with a folder
+        [Option("input", Required = false, HelpText = "Input path, file or directory.")]
+        public string InputPath { get; set; }
+
+        [Option("output", Required = false, HelpText = "Output path, file or directory.")]
+        public string OutputPath { get; set; }
     }
 }
